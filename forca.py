c12c2be086ce2172afb07508d9f65eb88e998d9e
welbertFC <welbert.jsj@gmail.com>
2019-08-03 03:29:08 -0300

    implementation of functions
    implementation of functions, for better code reading

--- forca.py
+++ forca.py
@@ -1,48 +1,26 @@
 import  random
+
 def jogo_forca():
 
-    print("***********************************")
-    print("Bem vindo ao jogo de forca")
-    print("***********************************")
+    mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
 
-
-    arquivo = open("palavra.txt", "r")
-    palavras = []
-
-    for linha in arquivo:
-        linha = linha.strip().upper()
-        palavras.append(linha)
-
-
-    arquivo.close()
-
-    numero = random.randrange(0,len(palavras))
-    palavra_secreta = palavras[numero].upper()
-
-    letras_acertadas = ["_" for letra in palavra_secreta]
-
- 
     enforcou = False
     acertou = False
     erros = 0
 
 
-    print(letras_acertadas)
-
-
     while(not enforcou and not  acertou):
 
-        chute = input("Qual a letra  ?")
-        chute = chute.strip().upper()
+        chute = pede_chute(chute,letras_acertadas,palavra_secreta)
 
 
         if (chute in palavra_secreta):
+            marca_chute_correto()
 
-            index = 0
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1
+
         else:
             print("ops você errou ainda falta {} tentativas".format(5-erros))
             erros += 1
@@ -63,3 +41,38 @@ def jogo_forca():
 
 if(__name__ == "__main__"):
     jogo_forca()
+
+
+def mensagem_abertura():
+    print("***********************************")
+    print("Bem vindo ao jogo de forca")
+    print("***********************************")
+
+def carrega_palavra_secreta():
+    arquivo = open("palavra.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip().upper()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra_secreta):
+    return ["_" for letra in palavra_secreta]
+
+def pede_chute():
+    chute =input("Qual a letra  ?")
+    chute = chute.strip().upper()
+
+def marca_chute_correto(chute,letras_acertadas,palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
